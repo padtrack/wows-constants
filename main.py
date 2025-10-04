dcec71f02d322f658d16cd4f457064b8f1f85014
Trackpad <60636294+padtrack@users.noreply.github.com>
2025-10-04 13:28:59 -0700

    add manifest file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import glob
 import json
 import os
 import shutil
@@ -11,3 +12,13 @@ with open(DUMP_PATH) as fp:
     version = constants["VERSION"]
     shutil.copy(DUMP_PATH, os.path.join(os.path.dirname(__file__), "data", "versions", f"{version['BUILD']}.json"))
     shutil.copy(DUMP_PATH, os.path.join(os.path.dirname(__file__), "data", f"latest.json"))
+
+manifest = {}
+for file in glob.iglob(os.path.join(os.path.dirname(__file__), "data", "versions", "*.json")):
+    with open(file) as fp:
+        data = json.load(fp)
+        version = data["VERSION"]
+        manifest[version["BUILD"]] = {"version": version["VERSION"], "patch": version["PATCH"]}
+
+with open("manifest.json", "w") as fp:
+    json.dump(dict(sorted(manifest.items(), key=lambda i: int(i[0]))), fp, indent=4)
